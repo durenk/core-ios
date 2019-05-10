cf3fa5d69c2ca18a9f26e7c19f5a5dd75d11fad7
fradenza <denza.dev@gmail.com>
2019-05-10 13:27:18 +0700

    add empty cell variation

--- OLCore/Classes/Views/SectionCollection/SectionCollection.swift
+++ OLCore/Classes/Views/SectionCollection/SectionCollection.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 private struct EmptySize {
+    static let XSmall = CGFloat(10)
     static let Small = CGFloat(20)
     static let Medium = CGFloat(30)
     static let Large = CGFloat(40)
+    static let XLarge = CGFloat(80)
+    static let XXLarge = CGFloat(120)
 }
 
 open class SectionCollection {
     private var contentView: TableView = TableView()
+    public var emptyXSmall: EmptySection {
+        get {
+            let section = EmptySection()
+            section.configure(contentView: contentView, height: EmptySize.XSmall)
+            return section
+        }
+    }
     public var emptySmall: EmptySection {
         get {
             let section = EmptySection()
@@ -34,6 +44,20 @@ open class SectionCollection {
         get {
             let section = EmptySection()
             section.configure(contentView: contentView, height: EmptySize.Large)
+            return section
+        }
+    }
+    public var emptyXLarge: EmptySection {
+        get {
+            let section = EmptySection()
+            section.configure(contentView: contentView, height: EmptySize.XLarge)
+            return section
+        }
+    }
+    public var emptyXXLarge: EmptySection {
+        get {
+            let section = EmptySection()
+            section.configure(contentView: contentView, height: EmptySize.XXLarge)
             return section
         }
     }
