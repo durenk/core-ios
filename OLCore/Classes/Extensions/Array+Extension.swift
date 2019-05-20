b37cf156a66bf0545ac40dcda1e7c9e45ddc2030
Aldo <aldo.onelabs@gmail.com>
2019-05-20 10:12:18 +0700

    add chunked func

---
+++ OLCore/Classes/Extensions/Array+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  Array+Extension.swift
+//  Alamofire
+//
+//  Created by ALDO LAZUARDI on 16/05/19.
+//
+
+import Foundation
+
+public extension Array {
+    public func chunked(into size: Int) -> [[Element]] {
+        return stride(from: 0, to: count, by: size).map {
+            Array(self[$0 ..< Swift.min($0 + size, count)])
+        }
+    }
+}
+
+
