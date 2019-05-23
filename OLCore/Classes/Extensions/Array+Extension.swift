15e30ef1151eb6c6b427b13e945f0c99310eb577
Aldo <aldo.onelabs@gmail.com>
2019-05-23 15:41:25 +0700

    code review

--- OLCore/Classes/Extensions/Array+Extension.swift
+++ OLCore/Classes/Extensions/Array+Extension.swift
@@ -9,6 +9,7 @@ import Foundation
 
 public extension Array {
     public func chunked(into size: Int) -> [[Element]] {
+        if size <= 0 { return [] }
         return stride(from: 0, to: count, by: size).map {
             Array(self[$0 ..< Swift.min($0 + size, count)])
         }
