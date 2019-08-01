17021f2c480fd49f6714b0db9db99adeeeb92b85
fradenza <denza.dev@gmail.com>
2019-08-01 13:21:11 +0700

    create CountryId for backend purpose

---
+++ OLCore/Classes/Constants/CountryId.swift
@@ -0,0 +1,12 @@
+//
+//  CountryId.swift
+//  Alamofire
+//
+//  Created by Sofyan Fradenza Adi on 01/08/19.
+//
+
+import Foundation
+
+public struct CountryId {
+    public static let Indonesia = "1HzzXcOkwxPpG3L0RiY3VVyn2V4"
+}
