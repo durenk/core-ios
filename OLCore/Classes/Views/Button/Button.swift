2a350dce0afbde5ef86fa0114500e18f0849b7ed
fradenza <denza.dev@gmail.com>
2019-08-23 09:21:12 +0700

    fix gradientLayer position

--- OLCore/Classes/Views/Button/Button.swift
+++ OLCore/Classes/Views/Button/Button.swift
@@ -80,7 +80,7 @@ open class Button: UIButton {
         gradientLayer.endPoint = style.buttonGradientEndPoint
         gradientLayer.colors = cgColors
         if gradientLayer.superlayer == nil {
-            layer.insertSublayer(gradientLayer, at: 0)
+            layer.insertSublayer(gradientLayer, at: 1)
         }
     }
 
