2dd4a85e27ed53e4b90410cdaa0d9be34af6095b
fradenza <denza.dev@gmail.com>
2019-09-13 14:52:51 +0700

    configurable automation to avoid keyboard

---
+++ OLCore/Classes/Constants/CoreConfig.swift
@@ -0,0 +1,14 @@
+//
+//  CoreConfig.swift
+//  OLCore
+//
+//  Created by Sofyan Fradenza Adi on 13/09/19.
+//
+
+import Foundation
+
+public struct CoreConfig {
+    public struct FormTableViewController {
+        public static var isAutoAvoidKeyboard: Bool = true
+    }
+}
