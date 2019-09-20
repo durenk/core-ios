f73e86409e362f2a17594a4fbac50b1fcb47f608
fradenza <denza.dev@gmail.com>
2019-09-20 18:52:48 +0700

    create pin pan view

--- OLCore/Classes/Views/PinInput/PinInputView.swift
+++ OLCore/Classes/Views/PinInput/PinInputView.swift
@@ -15,33 +15,75 @@ public enum PinInputType {
 open class PinInputView: View {
     private var type: PinInputType = .numeric
     private var length: Int = DefaultValue.emptyInt
-
-    convenience public init(type: PinInputType, length: Int) {
-        self.init(frame: CGRect.zero)
-        self.configure(type: type, length: length)
-    }
+    private var panViews: [PinPanView] = [PinPanView]()
+    private var panWidth: CGFloat = DefaultValue.emptyCGFloat
+    private var panSpacing: CGFloat = DefaultValue.emptyCGFloat
 
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        customInit()
     }
 
     override public init(frame: CGRect) {
         super.init(frame: frame)
-        customInit()
     }
 
-    private func customInit() {}
+    private func calculateActualPanWidth(containerWidth: CGFloat) -> CGFloat {
+        if panWidth > DefaultValue.emptyCGFloat {
+            return panWidth
+        }
+        if panSpacing == DefaultValue.emptyCGFloat { return DefaultValue.emptyCGFloat }
+        let numberOfSpacing = length - 1
+        let totalSpacing = panSpacing * CGFloat(numberOfSpacing)
+        let freeSpace = containerWidth - totalSpacing
+        return freeSpace / CGFloat(length)
+    }
+
+    private func calculateActualPanSpacing(containerWidth: CGFloat) -> CGFloat {
+        if panWidth <= DefaultValue.emptyCGFloat {
+            return panSpacing
+        }
+        if panWidth == DefaultValue.emptyCGFloat { return DefaultValue.emptyCGFloat }
+        let numberOfSpacing = length - 1
+        let totalPanWidth = panWidth * CGFloat(length)
+        let freeSpace = containerWidth - totalPanWidth
+        return freeSpace / CGFloat(numberOfSpacing)
+    }
+
+    private func renderPinViews(containerSize: CGSize) {
+        panViews.removeAll()
+        let panWidth = calculateActualPanWidth(containerWidth: containerSize.width)
+        let panSpacing = calculateActualPanSpacing(containerWidth: containerSize.width)
+        for index in 0...(length - 1) {
+            let frame = CGRect(
+                x: (panWidth * CGFloat(index)) + (panSpacing * CGFloat(index)),
+                y: DefaultValue.emptyCGFloat,
+                width: panWidth,
+                height: containerSize.height
+            )
+            let panView = PinPanView(frame: frame)
+            panView.render()
+            panViews.append(panView)
+            addSubview(panView)
+        }
+    }
 
     private func render() {
         self.layoutIfNeeded()
         let containerSize = bounds.size
         if containerSize.width == DefaultValue.emptyCGFloat { return }
+        renderPinViews(containerSize: containerSize)
     }
 
-    public func configure(type: PinInputType, length: Int) {
+    public func configure(
+        type: PinInputType,
+        length: Int,
+        panWidth: CGFloat = DefaultValue.emptyCGFloat,
+        panSpacing: CGFloat = DefaultValue.emptyCGFloat
+    ) {
         self.type = type
         self.length = length
+        self.panWidth = panWidth
+        self.panSpacing = panSpacing
         self.render()
     }
 }
---
+++ OLCore/Classes/Views/PinInput/PinPanView.swift
@@ -0,0 +1,16 @@
+//
+//  PinView.swift
+//  OLCore
+//
+//  Created by Sofyan Fradenza Adi on 20/09/19.
+//
+
+import UIKit
+
+class PinPanView: View {
+    var pinLabel: UILabel = UILabel()
+
+    func render() {
+        backgroundColor = .red
+    }
+}
