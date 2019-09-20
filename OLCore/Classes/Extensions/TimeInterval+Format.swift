e1bd5abb4f5aa630415e352ef082c066411dacb8
fradenza <denza.dev@gmail.com>
2019-09-20 08:59:02 +0700

    add custom separator

--- OLCore/Classes/Extensions/TimeInterval+Format.swift
+++ OLCore/Classes/Extensions/TimeInterval+Format.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 extension TimeInterval {
-    public func convertToHumanTextFormat() -> String {
+    public func convertToHumanTextFormat(separator: String = ":") -> String {
         let minutes = Int(self) / 60
         let seconds = Int(self) % 60
-        return String(format: "%02d:%02d", minutes, seconds)
+        return String(format: "%02d%@%02d", minutes, separator, seconds)
     }
 }
