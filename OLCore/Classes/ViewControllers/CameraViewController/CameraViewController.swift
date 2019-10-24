f85d7fbf2da603af08baaeb17c89c53501ce3ff6
fradenza <denza.dev@gmail.com>
2019-10-24 17:35:55 +0700

    create CameraViewController

---
+++ OLCore/Classes/ViewControllers/CameraViewController/CameraViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CameraViewController.swift
+//  OLCore
+//
+//  Created by Sofyan Fradenza Adi on 24/10/19.
+//
+
+import UIKit
+
+class CameraViewController: ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("===")
+    }
+}
