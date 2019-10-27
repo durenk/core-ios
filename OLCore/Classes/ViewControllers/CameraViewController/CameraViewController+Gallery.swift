b2b4b78a3e91e9045ba4ed6ab5b27cad5365d35c
fradenza <fradenza@gmail.com>
2019-10-27 22:26:50 +0700

    import from gallery

---
+++ OLCore/Classes/ViewControllers/CameraViewController/CameraViewController+Gallery.swift
@@ -0,0 +1,24 @@
+//
+//  CameraViewController+Gallery.swift
+//  OLCore
+//
+//  Created by DENZA on 27/10/19.
+//
+
+import Foundation
+
+extension CameraViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    public func importFromGallery() {
+        let imagePicker = UIImagePickerController()
+        if !UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum) { return }
+        imagePicker.delegate = self
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+    public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        delegate?.cameraViewControllerDidTakeImage(image)
+    }
+}
