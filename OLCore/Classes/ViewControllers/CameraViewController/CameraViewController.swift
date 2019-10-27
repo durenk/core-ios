5fec96057a70f69d7c60359c689819f892b3b674
fradenza <fradenza@gmail.com>
2019-10-27 21:51:56 +0700

    flashlight

--- OLCore/Classes/ViewControllers/CameraViewController/CameraViewController.swift
+++ OLCore/Classes/ViewControllers/CameraViewController/CameraViewController.swift
@@ -122,4 +122,35 @@ open class CameraViewController: TableViewController {
             }
         }
     }
+
+    public func isFlashlightOn() -> Bool {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return false }
+        guard device.hasTorch else { return false }
+        do {
+            try device.lockForConfiguration()
+            return device.torchMode == AVCaptureDevice.TorchMode.on
+        } catch {
+            return false
+        }
+    }
+
+    public func turnOnFlashlight(_ isTurnOn: Bool) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { return }
+        do {
+            try device.lockForConfiguration()
+            if isTurnOn {
+                do {
+                    try device.setTorchModeOn(level: 1.0)
+                } catch {
+                    print(error)
+                }
+            } else {
+                device.torchMode = AVCaptureDevice.TorchMode.off
+            }
+            device.unlockForConfiguration()
+        } catch {
+            print(error)
+        }
+    }
 }
