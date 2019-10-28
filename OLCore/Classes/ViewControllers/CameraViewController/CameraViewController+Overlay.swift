23d7e8c91e4ac19d5d1f6a1bb5e2cbc70f1815fe
fradenza <fradenza@gmail.com>
2019-10-28 23:39:19 +0700

    render overlay

--- OLCore/Classes/ViewControllers/CameraViewController/CameraViewController+Overlay.swift
+++ OLCore/Classes/ViewControllers/CameraViewController/CameraViewController+Overlay.swift
@@ -5,7 +5,7 @@
 //  Created by DENZA on 28/10/19.
 //
 
-import Foundation
+import UIKit
 
 extension CameraViewController {
     public func addOverlayIso7810Id1(
@@ -14,7 +14,7 @@ extension CameraViewController {
         overlayAlpha: CGFloat = 0.4
     ) {
         let width = SizeHelper.getWidth(
-            containerWidth: view.bounds.size.width,
+            containerWidth: contentView.bounds.size.width,
             horizontalPadding: horizontalPadding
         )
         let height = SizeHelper.getHeightOfScale(
@@ -24,15 +24,25 @@ extension CameraViewController {
         )
         let originX = SizeHelper.getOriginXAlignCenter(
             width: width,
-            containerWidth: view.bounds.size.width
+            containerWidth: contentView.bounds.size.width
         )
         let originY = SizeHelper.getOriginYAlignCenter(
             height: height,
-            containerHeight: view.bounds.size.height
+            containerHeight: contentView.bounds.size.height
         )
-        print("===width:", width)
-        print("===height:", height)
-        print("===originX:", originX)
-        print("===originY:", originY)
+        let overlayPath = UIBezierPath(rect: contentView.bounds)
+        let transparentPath = UIBezierPath(rect: CGRect(
+            x: originX,
+            y: originY,
+            width: width,
+            height: height
+        ))
+        overlayPath.append(transparentPath)
+        overlayPath.usesEvenOddFillRule = true
+        let fillLayer = CAShapeLayer(layer: contentView.layer)
+        fillLayer.path = overlayPath.cgPath
+        fillLayer.fillRule = CAShapeLayerFillRule.evenOdd
+        fillLayer.fillColor = overlayColor.withAlphaComponent(overlayAlpha).cgColor
+        contentView.layer.addSublayer(fillLayer)
     }
 }
