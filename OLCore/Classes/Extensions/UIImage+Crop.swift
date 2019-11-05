0f5d2a55a30897a56d68ea0eae195f144d0a75ae
fradenza <denza.dev@gmail.com>
2019-11-05 16:46:33 +0700

    scaledImage

--- OLCore/Classes/Extensions/UIImage+Crop.swift
+++ OLCore/Classes/Extensions/UIImage+Crop.swift
@@ -36,4 +36,18 @@ extension UIImage {
         }
         return self
     }
+
+    public func scaledImage(maxDimension: CGFloat) -> UIImage {
+        var scaledSize = CGSize(width: maxDimension, height: maxDimension)
+        if size.width > size.height {
+            scaledSize.height = size.height / size.width * scaledSize.width
+        } else {
+            scaledSize.width = size.width / size.height * scaledSize.height
+        }
+        UIGraphicsBeginImageContext(scaledSize)
+        draw(in: CGRect(origin: .zero, size: scaledSize))
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return scaledImage ?? self
+    }
 }
