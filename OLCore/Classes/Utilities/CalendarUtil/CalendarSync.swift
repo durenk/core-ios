671fca885086f26ccc9afd94ffd68337eef00e42
nicko <nickojyj@gmail.com>
2019-11-07 15:04:28 +0700

    add syncWithCalendarDetails

--- OLCore/Classes/Utilities/CalendarUtil/CalendarSync.swift
+++ OLCore/Classes/Utilities/CalendarUtil/CalendarSync.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import EventKit
+import EventKitUI
 
 open class CalendarSync{
     
@@ -28,6 +29,32 @@ open class CalendarSync{
                     completion(true)
                 } catch {
                     completion(false)
+                }
+            }
+        }
+    }
+
+    public static func syncToCalendarDetails(
+        eventCalendar: EventCalendar,
+        completion: @escaping (_ eventViewController: EKEventEditViewController) -> Void
+    ) {
+        let eventStore: EKEventStore = EKEventStore()
+        eventStore.requestAccess(to: .event) { (granted, error) in
+            if granted && error == nil {
+                let event: EKEvent = EKEvent(eventStore: eventStore)
+                event.title = eventCalendar.title
+                event.startDate = eventCalendar.startDate
+                event.endDate = eventCalendar.endDate
+                event.location = eventCalendar.location
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                let eventViewController: EKEventEditViewController = EKEventEditViewController()
+                eventViewController.event = event
+                eventViewController.eventStore = eventStore
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                    completion(eventViewController)
+                } catch {
+                    completion(eventViewController)
                 }
             }
         }
