17457a07a6b12c787a0a6e91bab4131936794511
Aldo <aldo.onelabs@gmail.com>
2019-11-14 17:49:35 +0700

    code review

--- OLCore/Classes/ViewControllers/PreviewFileViewController/PreviewFile.swift
+++ OLCore/Classes/ViewControllers/PreviewFileViewController/PreviewFile.swift
@@ -9,7 +9,7 @@ import UIKit
 
 open class PreviewFile: NSObject, UIDocumentInteractionControllerDelegate {
     private var controller: UIViewController!
-    private var documentInteractionController: UIDocumentInteractionController!
+    private var documentInteractionController: UIDocumentInteractionController?
 
     public init(controller: UIViewController) {
         super.init()
@@ -18,8 +18,8 @@ open class PreviewFile: NSObject, UIDocumentInteractionControllerDelegate {
 
     public func previewFile(path: URL)  {
         documentInteractionController = UIDocumentInteractionController(url: path)
-        documentInteractionController.delegate = self
-        documentInteractionController.presentPreview(animated: true)
+        documentInteractionController?.delegate = self
+        documentInteractionController?.presentPreview(animated: true)
     }
 
     public func documentInteractionControllerViewControllerForPreview(_ controller: UIDocumentInteractionController) -> UIViewController {
