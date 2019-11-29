a826187627bc51caad1cc758f199b1a7d68bc3d2
Aldo <aldo.onelabs@gmail.com>
2019-11-29 14:31:34 +0700

    rotate infinite uiview

--- OLCore/Classes/Extensions/UIView+Extension.swift
+++ OLCore/Classes/Extensions/UIView+Extension.swift
@@ -134,4 +134,23 @@ extension UIView {
         }
         return textField
     }
+    
+    private static let kRotationAnimationKey = "rotationanimationkey"
+    
+    public func rotate(duration: Double = 1) {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) == nil {
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = Float.pi * 2.0
+            rotationAnimation.duration = duration
+            rotationAnimation.repeatCount = Float.infinity
+            layer.add(rotationAnimation, forKey: UIView.kRotationAnimationKey)
+        }
+    }
+    
+    public func stopRotating() {
+        if layer.animation(forKey: UIView.kRotationAnimationKey) != nil {
+            layer.removeAnimation(forKey: UIView.kRotationAnimationKey)
+        }
+    }
 }
